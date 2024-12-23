3e8a1dc2ab73d58593b3a7a165f02daf7e66b616
BugHunter-X <askzenai@gmail.com>
2024-12-23 09:28:05 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,11 @@
+function printNumber(num: number): void {
+  console.log(num);
+}
+
+function calculate(a: number, b: number): number {
+  const result = a + b; 
+  printNumber(result);
+  return result;
+}
+
+const result = calculate(5, '10'); // Type Error
